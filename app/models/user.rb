e3df9b77b7051a21a7fcbe201c04a6b0d2007479
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-11 19:11:00 +0900

    正規表現の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
 end
