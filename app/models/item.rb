bf1ab461b9d5bfa33ace3237c8c6cf6c8e25ea3e
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-12 19:26:48 +0900

    ActiveHashのアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :product_condition
+  belongs_to :delivery_fee
+  belongs_to :delivery_area
+  belongs_to :delivery_days
 
-  validates :category_id, numericality: { other_than: 1 } 
+  validates :category_id, :product_condition_id, :delivery_fee_id, :delivery_area_id, :delivery_days_id, numericality: { other_than: 1 } 
 end
