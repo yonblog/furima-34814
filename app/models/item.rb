b822ea4231aa52668ca29d2fd40ba4215e4d157b
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-12 17:59:40 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :category_id, numericality: { other_than: 1 } 
 end
