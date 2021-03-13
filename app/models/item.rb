8928da8e526f87d919ce77ac43353b7ccf8030e0
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-13 23:03:59 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,20 @@ class Item < ApplicationRecord
   belongs_to :delivery_days
 
   validates :category_id, :product_condition_id, :delivery_fee_id, :delivery_area_id, :delivery_days_id, numericality: { other_than: 1 } 
+  
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :product_description
+    validates :category_id
+    validates :product_condition_id
+    validates :delivery_fee_id
+    validates :delivery_area_id
+    validates :delivery_days_id
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, allow_blank: true
+  end
+
+  def was_attached?
+    self.image.attached?
+  end
 end
