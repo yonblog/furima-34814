6109367ea643c066468efba2890ed4b30a738d39
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-13 23:05:51 +0900

    商品出品機能のコントローラーの実装
    newアクション
    createアクション
    ストロングパラメーター
    ログインしていない場合のリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:new]
   def index
-    
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :product_description, :category_id, :product_condition_id, :delivery_fee_id, :delivery_area_id, :delivery_days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
