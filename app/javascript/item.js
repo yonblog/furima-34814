d3882e4a8d96495cb6624335fdc2f8de540af55d
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-15 16:53:59 +0900

    販売手数料、販売利益のconsole.logの削除

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -2,17 +2,13 @@ function post (){
   const priceForm = document.getElementById("item-price");
   priceForm.addEventListener('input', function(){
     const inputData = priceForm.value;
-    console.log(inputData);
 
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = (Math.floor(inputData * 0.1));
-    console.log(addTaxPrice);
     
     const profitPrice = document.getElementById("profit");
     const extractingNumber = inputData * 0.1;
-    console.log(extractingNumber);
     profitPrice.innerHTML = (Math.floor(inputData - extractingNumber));
-    console.log(profitPrice);
   });
 }
 
