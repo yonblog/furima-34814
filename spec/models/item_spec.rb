4e1ca0fce8f151ef2969e1e2409a24c00efe83f1
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-15 18:19:17 +0900

    記述のミスの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -58,12 +58,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'priceが300円未満では出品できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it 'priceが10,000,000円以上では出品できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
