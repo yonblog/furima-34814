6a89ec7d810e5cf4f1512869a4fd3ff59bea9381
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-15 16:55:34 +0900

    itemモデル単体テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('app/assets/images/test.jpeg')
+  end
+  describe '商品出品' do
+    context '商品が出品できる場合' do
+      it 'image,product_name,product_description,category_id,product_condition_id,delivery_fee_id,delivery_area_id,delivery_days_id,priceが存在していて、バリデーション通りに入力されていれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品が出品できない場合' do
+      it 'imageが空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'product_nameが空では出品できない' do
+        @item.product_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product name can't be blank")
+      end
+      it 'product_descriptionが空では出品できない' do
+        @item.product_description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product description can't be blank")
+      end
+      it 'category_idが「1」では出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'product_condition_idが「1」では出品できない' do
+        @item.product_condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product condition must be other than 1")
+      end
+      it 'delivery_fee_idが「1」では出品できない' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
+      end
+      it 'delivery_area_idが「1」では出品できない' do
+        @item.delivery_area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery area must be other than 1")
+      end
+      it 'delivery_days_idが「1」では出品できない' do
+        @item.delivery_days_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery days must be other than 1")
+      end
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが300円未満では出品できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが10,000,000円以上では出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
+end  
