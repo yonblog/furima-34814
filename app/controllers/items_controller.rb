05283e77707658670a6a433fa8b0f90fc65cdff1
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-16 11:39:02 +0900

    商品情報が更新できるようにする実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
