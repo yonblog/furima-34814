bb3ab3295c38f30822c2f3cd3467a4daa3e98a9a
yonblog <77332211+yonblog@users.noreply.github.com>
2021-03-18 21:54:01 +0900

    エラーメッセージ重複表示の解消の処理

--- app/models/personal_purchasing.rb
+++ app/models/personal_purchasing.rb
@@ -3,15 +3,18 @@ class PersonalPurchasing
   attr_accessor :postal_code, :delivery_area_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Please enter the correct number and hyphen (-)"}
+    validates :postal_code
     validates :delivery_area_id, numericality: { other_than: 1, message: "Select" } 
     validates :municipality
     validates :address
-    validates :phone_number, format: {with: /\A\d{10}$|^\d{11}\z/, message: "Enter a 10- or 11-digit number"}
+    validates :phone_number
     validates :user_id
     validates :item_id
     validates :token
   end
+
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Please enter the correct number and hyphen (-)"}, allow_blank: true
+  validates :phone_number, format: {with: /\A\d{10}$|^\d{11}\z/, message: "Enter a 10- or 11-digit number"}, allow_blank: true
 
   def save
     purchasing_management = PurchasingManagement.create(user_id: user_id, item_id: item_id)
